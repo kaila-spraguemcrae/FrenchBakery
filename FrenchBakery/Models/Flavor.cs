b6936910c7849f4c9f5e35c1d3c98fe7b8377e44
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-15 10:03:29 -0800

    add models

--- FrenchBakery/Models/Flavor.cs
+++ FrenchBakery/Models/Flavor.cs
@@ -1,7 +1,15 @@
+using System.Collections.Generic;
+
 namespace FrenchBakery.Models
 {
   public class Flavor
   {
-    
+    public Flavor()
+    {
+      this.Treats = new HashSet<FlavorTreat>();
+    }
+    public int FlavorId { get; set; }
+    public string Type { get; set; }
+    public ICollection<FlavorTreat> Treats { get; }
   }
 }
--- FrenchBakery/Models/Treat.cs
+++ FrenchBakery/Models/Treat.cs
@@ -1,7 +1,16 @@
+using System.Collections.Generic;
+
 namespace FrenchBakery.Models
 {
   public class Treat
   {
-    
+    public Treat()
+    {
+      this.Flavors = new HashSet<FlavorTreat>();
+    }
+    public int TreatId { get; set; }
+    public string Name { get; set; }
+    public float Price { get; set; }
+    public ICollection<FlavorTreat> Flavors { get; }
   }
 }
