8ab469ba74a3a33be5ec32391c80bc893cbbfd83
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-15 16:41:07 -0800

    add migration for ExtendIdentityUser, adds First and Last Name to user

--- FrenchBakery/Controllers/AccountController.cs
+++ FrenchBakery/Controllers/AccountController.cs
@@ -34,7 +34,13 @@ namespace FrenchBakery.Controllers
     [HttpPost]
     public async Task<ActionResult> Register (RegisterViewModel model)
     {
-      var user = new ApplicationUser { UserName = model.Email };
+      var user = new ApplicationUser 
+      { 
+        UserName = model.Email,
+        Email = model.Email,
+        FirstName = model.FirstName,
+        LastName = model.LastName
+      };
       IdentityResult result = await _userManager.CreateAsync(user, model.Password);
       if (result.Succeeded)
       {
