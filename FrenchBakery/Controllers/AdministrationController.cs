b13cdcfe0c3d8bfae1972087c60b8802bc7a371a
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-16 15:08:39 -0800

    add eEdittRole view and controller w/ bootstrap styling

--- FrenchBakery/Controllers/AdministrationController.cs
+++ FrenchBakery/Controllers/AdministrationController.cs
@@ -10,10 +10,12 @@ namespace FrenchBakery.Controllers
   public class AdministrationController: Controller
   {
     private readonly RoleManager<IdentityRole> roleManager;
+    private readonly UserManager<ApplicationUser> userManager;
 
-    public AdministrationController(RoleManager<IdentityRole> roleManager)
+    public AdministrationController(RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
     {
       this.roleManager = roleManager;
+      this.userManager = userManager;
     }
     public IActionResult CreateRole()
     {
@@ -47,6 +49,27 @@ namespace FrenchBakery.Controllers
     {
       var roles = roleManager.Roles;
       return View(roles);
+    }
+
+    public async Task<IActionResult> EditRole(string id)
+    {
+      var role = await roleManager.FindByIdAsync(id);
+
+      // if(role == null)
+      // {
+      //   ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
+      //   return View("NotFound");
+      // }
+      var model = new EditRoleViewModel {Id = role.Id, RoleName = role.Name};
+      
+      foreach (var user in userManager.Users)
+      {
+        if(await userManager.IsInRoleAsync(user, role.Name))
+        {
+          model.Users.Add(user.UserName);
+        }
+      }
+      return View(model);
     }
   }
 }
