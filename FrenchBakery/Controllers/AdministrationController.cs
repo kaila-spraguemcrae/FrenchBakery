0e4dda0a2889c95ab0710b58286f470a74f065d2
Kaila Sprague McRae <kaila.sprague@icloud.com>
2021-01-16 16:33:27 -0800

    add EditUserInRole view and controller

--- FrenchBakery/Controllers/AdministrationController.cs
+++ FrenchBakery/Controllers/AdministrationController.cs
@@ -2,6 +2,7 @@ using FrenchBakery.Models;
 using FrenchBakery.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
 
@@ -55,11 +56,6 @@ namespace FrenchBakery.Controllers
     {
       var role = await roleManager.FindByIdAsync(id);
 
-      // if(role == null)
-      // {
-      //   ViewBag.ErrorMessage = $"Role with Id = {id} cannot be found";
-      //   return View("NotFound");
-      // }
       var model = new EditRoleViewModel {Id = role.Id, RoleName = role.Name};
       
       foreach (var user in userManager.Users)
@@ -77,12 +73,69 @@ namespace FrenchBakery.Controllers
     {
       var role = await roleManager.FindByIdAsync(model.Id);
       role.Name = model.RoleName;
+
       var result = await roleManager.UpdateAsync(role);
+
       if(result.Succeeded)
       {
         return RedirectToAction("ListRoles");
       }
       return View(model);
+    }
+
+    public async Task<IActionResult> EditUsersInRole(string id)
+    {
+        ViewBag.roleId = id;
+
+        var role = await roleManager.FindByIdAsync(id);
+
+        var model = new List<UserRoleViewModel>();
+
+        foreach (var user in userManager.Users)
+        {
+            var userRoleViewModel = new UserRoleViewModel
+            {
+                UserId = user.Id,
+                UserName = user.UserName,
+                IsSelected = await userManager.IsInRoleAsync(user, role.Name)
+            };
+
+            // if (await userManager.IsInRoleAsync(user, role.Name))
+            // {
+            //     userRoleViewModel.IsSelected = true;
+            // }
+            // else
+            // {
+            //     userRoleViewModel.IsSelected = false;
+            // }
+
+            model.Add(userRoleViewModel);
+        }
+
+        return View(model);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> EditUsersInRole(List<UserRoleViewModel> model, string id)
+    {
+      var role = await roleManager.FindByIdAsync(id);
+
+      for(int i = 0; i < model.Count; i++)
+      {
+        var user = await userManager.FindByIdAsync(model[i].UserId);
+
+        IdentityResult result = null;
+
+        if(model[i].IsSelected && (await userManager.IsInRoleAsync(user, role.Name)))
+        {
+          result = await userManager.AddToRoleAsync(user, role.Name);
+        }
+        else if(!model[i].IsSelected && (await userManager.IsInRoleAsync(user, role.Name)))
+        {
+          result = await userManager.RemoveFromRoleAsync(user, role.Name);
+        }
+      }
+      return View();
     }
   }
 }
